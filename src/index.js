4bcf2784e02dc5a27ec2808e9f5959bb82b7df47
jallusparta <aleksi.a.partanen@aalto.fi>
2018-11-15 14:59:30 +0200

    React app created and the app's displaying hello world.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+
+ReactDOM.render(<App />, document.getElementById('root'));
