33da472f67d98c1db6f0ddd7ed4cf9265bff8afa
jallusparta <aleksi.a.partanen@aalto.fi>
2018-11-29 17:28:09 +0200

    Implemented a button that changes state when pushed, and displays a question stored in the state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+//import GetQuestions from './service';
 
-const App = () => (
-    <div>
-      <p>Hello world</p>
-    </div>
-  )
 
+const Button = ({ klikki, nimi }) => (
+  <button onClick = {klikki} >{nimi}</button>
+)
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hyva: 0,
+      question: "Who let the dogs out?"
+    }
+  }
+
+  klikHyva = () => {
+    this.setState({
+      hyva: this.state.hyva + 1
+    })
+  }
+
+  render () {
+    const otsake = 'Anna palautetta'
+    const aliotsake = 'Statistiikka'
+    const statistiikka = () => {
+      if ((this.state.hyva) === 0) {
+        return (
+          <div>
+            <em>Button not yet pushed</em>
+          </div>
+        )
+      }
+      return (
+        <div>{this.state.question}</div> 
+      )
+    }
+
+    return (
+      <div>
+        <Button klikki = {this.klikHyva} nimi = "Hyvä" />
+
+        <div>{statistiikka()}</div>
+      </div>
+    )
+  }
+}
+
+export default App
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/service.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const baseUrl = 'https://app-staging.incy.io/api/bitsaber-staging/observation-questions/links/staging-place-tarvikkeet'
+
+const GetQuestions = () => {
+    return (
+        <div>
+            <p>This should display the question when implemented</p>
+        </div>
+    )
+}
+
+export default App
+
+<button onClick = {<ClickAndGet state = {this.state} />}  >Don't Push</button>
+<div>{question()}</div>
