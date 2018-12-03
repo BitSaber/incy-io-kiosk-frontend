1f26e041b0f2b4d19c82a545f7bd62ddf675a435
Axel Latvala <axel@latvala.com>
2018-12-03 17:04:54 +0200

    structural improvement

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Hello world</p>
+            </div>
+        )
+    }
+}
+
+export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-
-class App extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div>
-                <p>Hello world</p>
-            </div>
-        )
-    }
-}
+import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('app'));
