554d3ef9745d375df2b31fd62a81caecc63d22b9
Axel Latvala <axel@latvala.com>
2018-12-04 22:25:46 +0200

    Example of how to use axios to display data

--- src/App.jsx
+++ src/App.jsx
@@ -6,57 +6,71 @@ import axios from 'axios'
 const observationQuestionUrl = 'https://app-staging.incy.io/api/bitsaber-staging/observation-questions/links/staging-place-tarvikkeet';
 const element = <button className="myButton">Yes</button>
 const element1 = <button className="myButton1" onClick={ActionLink}>No</button>
-const q = GetQuestion.apply(7366).then(json => json.data.data.name).catch(error => alert(error))
-const question = <h1>{q}</h1>  // Insert the API question here
 
 
-const getQuestions = () => (
-    axios
+function getQuestions() {
+    return axios
         .get(observationQuestionUrl)
-)
-
-const GetQuestion = () => {
-    const promise = getQuestions()
-    // console.log(promise)
-    return (
-        promise.then(response => {
-            return response.data.data
-        })
-    )
 }
-
-
 
 class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            errorOccurred: false,
+            errorMessage: null,
+            loading: false,
+            questions: []
+        }
+        getQuestions()
+            .then(response => {
+                this.setState({
+                    loading: false,
+                    errorOccurred: false,
+                    errorMessage: null,
+                    questions: response.data.data
+                })
+            })
+            .catch(error => {
+                this.setState({
+                    loading: false,
+                    errorOccurred: true,
+                    errorMessage: error
+                })
+            })
     }
+
 
     render() {
 
+        const questionElems = []
+        this.state.questions.forEach((value, key) => {
+            console.log(key, value) // eslint-disable-line
+            questionElems.push(
+                <li key={'question_'+value.id}>{value.name}</li>
+            )
+        })
+
         return (
             // Insert API answers
-            <body>
+            <div className="center-align">
+                <div className="txt">
+                    <h1>
+                        <ul>
+                            {questionElems}
+                        </ul>
+                    </h1>
+                </div>
+                {this.state.errorOccurred ? this.state.errorMessage : 'All\'s good in the neighbourhood!'}
 
-                <div className="center-align">
-                    <div className="txt" >
-                        {question}
-                    </div>
-
-                    <div>
-                        {element}
-                        {element1}
-                    </div>
-
+                <div>
+                    {element}
+                    {element1}
                 </div>
 
-            </body>
+            </div>
         )
-
-
     }
-
-
 }
 
 function ActionLink(event) {
