c499444f3dd7480f710e4c6c813898ea2f08afcd
Sebastian Berner <basti_@hotmail.com>
2018-12-04 19:42:25 +0200

    Added thank you page and return to inital state after 3 seconds

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,34 @@ import Typography from '@material-ui/core/Typography'
 import './index.css'
 import Button from '@material-ui/core/Button'
 
+const initialState = {
+    counter: 1,
+    currentId: 5090,
+    observationQuestions: [],
+    questionsChoices: [],
+    observationAnswer: []
+}
 
 class App extends Component {
     constructor(props) {
         super(props)
-        this.state = {
-            counter: 0,
-            currentId: 5090,
-            observationQuestions: [],
-            questionsChoices: []
-        }
+        this.state = initialState
+        this.initializeQuestions = this.initializeQuestions.bind(this)
+        this.initializeQuestions()
     }
 
     clickGet() {
         this.setState({
             counter: this.state.counter + 1,
         });
+        GetQuestion().then((questions) => {
+            this.setState({
+                observationQuestions: questions
+            })
+        })
+    }
+
+    initializeQuestions() {
         GetQuestion().then((questions) => {
             this.setState({
                 observationQuestions: questions
@@ -36,8 +48,25 @@ class App extends Component {
         })
     }
 
-    render() {
+    clickHandler(answer) {
+        const count = this.state.counter
+        const obsAns = this.state.observationAnswer
+        this.setState ({
+            counter: count + 1,
+            observationAnswer: [...obsAns].concat(answer)
+        })
+        console.log(obsAns) // eslint-disable-line
+    }
 
+    submitObservation() {
+        //TODO: Post observation
+        setTimeout(() => {
+            this.setState(initialState)
+            this.initializeQuestions()
+        }, 3000)
+    }
+
+    render() {
         const TestButton = ({ clickAction, name }) => (
             <Button
                 variant="contained"
@@ -47,20 +76,15 @@ class App extends Component {
                 {name}
             </Button>
         )
+        const counter = this.state.counter;
+        console.log(counter) // eslint-disable-line
         const displayedText = () => {
-            if (this.state.counter === 0) {
-                return (
-                    <div>
-                        <Typography variant='h1'>Button not yet pushed</Typography>
-                    </div>
-                )
-            }
             if (this.state.observationQuestions.length !== 0) {
                 return (
                     <div>
                         <Typography variant='h1'>
                             {this.state.observationQuestions.find(function(question) {
-                                return question.position === 1;
+                                return question.position === counter;
                             }).name}
                         </Typography>
                     </div>
@@ -68,13 +92,24 @@ class App extends Component {
                 )
             }
         }
-        return (
-            <div>
-                <div>{displayedText()}</div>
-                <TestButton clickAction = {this.clickGet.bind(this)} name = "GET" />
-                <TestButton clickAction = {this.clickChoices.bind(this)} name = "Choices" />
-            </div>
-        )
+        if (this.state.observationQuestions.length === 0) {
+            return null;
+        }
+        else if (this.state.counter > this.state.observationQuestions.length) {
+            this.submitObservation()
+            return (<div>
+                Kiitos palautteesta!
+            </div>)
+        }
+        else {
+            return (
+                <div>
+                    <div>{displayedText()}</div>
+                    <TestButton clickAction = {() => this.clickHandler('Kyllä')} name = "Kyllä" />
+                    <TestButton clickAction = {() => this.clickHandler('Ei')} name = "Ei" />
+                </div>
+            )
+        }
     }
 }
 
