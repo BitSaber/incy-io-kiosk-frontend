2c3625ea533a9a6c46cf8a0d9fe3c79ccc064b1f
Axel Latvala <axel@latvala.com>
2018-12-07 16:02:10 +0200

    Refactoring and fixing so that this actually compiles

--- src/pages/QuestionPage.jsx
+++ src/pages/QuestionPage.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types'
 
 import BigButton from '../components/BigButton';
 
-const QuestionPage = ({ question, questionChoices, onChoiceClick }) => (
-    <div>
-        <Typography variant='h1'>{question.name}</Typography>
-        <Grid container justify="center">
-            {questionChoices.map(questionsChoice => (
-                <BigButton
-                    key={questionsChoice.id}
-                    onClick={() => onChoiceClick(questionsChoice)}
-                    text={questionsChoice.name}
-                />
-            ))
-            }
-        </Grid>
-    </div>
-);
+class QuestionPage extends Component {
+    static propTypes = {
+        question: PropTypes.object.isRequired,
+        questionChoices: PropTypes.arrayOf(PropTypes.object).isRequired,
+        onChoiceClick: PropTypes.func.isRequired
+    }
+    render() {
+        return (
+            <div>
+                <Typography variant='h1'>{this.props.question.name}</Typography>
+                <Grid container justify="center">
+                    {this.props.questionChoices.map(questionsChoice => (
+                        <BigButton
+                            key={questionsChoice.id}
+                            onClick={() => this.props.onChoiceClick(questionsChoice)}
+                            text={questionsChoice.name}
+                        />
+                    ))
+                    }
+                </Grid>
+            </div>
+        )
+    }
+}
 
 export default QuestionPage;
