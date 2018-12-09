df2d914e615eb58c8d8603b18eeec657c2d70bf9
khenriksson <kasper.henriksson@aalto.fi>
2018-12-09 18:46:51 +0200

    Initial design implemented to working stage

--- src/pages/ThankYouPage.jsx
+++ src/pages/ThankYouPage.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 
 const ThankYouPage = () => (
-    <Typography variant="h1">Kiitos!</Typography>
+    <div>
+        <div className="center-align"><img src="/planblogo_color.jpg" className="logo"></img> </div>
+        <div className="answer-div">
+            <Typography class="txt"><h2>Kiitos vastaamisesta!</h2></Typography>
+        </div>
+
+        <footer className="footer">
+            <footer className="inside">
+                <p>Copyright © 2018 BitSaber, Otaniemi, Finland</p>
+                <div className="under">
+                    <ul>
+                        <li> <a href="https://github.com/BitSaber/incy-io-kiosk-frontend" target="_blank" rel="noopener noreferrer">GitHub</a> </li>
+                    </ul>
+                </div>
+            </footer>
+        </footer>
+
+    </div>
 );
 
 export default ThankYouPage;
