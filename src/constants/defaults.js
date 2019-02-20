a1d77c1b3837627c1ee28303ce448370ce37d47a
jallusparta <aleksi.a.partanen@aalto.fi>
2019-02-20 15:30:16 +0200

    Fixed defaults, that were incorrected for testing.

--- src/constants/defaults.js
+++ src/constants/defaults.js
@@ -1,3 +1,3 @@
-export const DEFAULT_ORG_NAME = 'bitsaber-stagin';
-export const DEFAULT_LINK_NAME = 'staging-place-tarvikkee';
-export const DEFAULT_BASE_API_URL = 'https://app-staging.incy.io/ap';
+export const DEFAULT_ORG_NAME = 'bitsaber-staging';
+export const DEFAULT_LINK_NAME = 'staging-place-tarvikkeet';
+export const DEFAULT_BASE_API_URL = 'https://app-staging.incy.io/api';
