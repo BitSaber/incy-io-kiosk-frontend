5aee0e786185725968abaf154049fcbd8599b86f
jallusparta <aleksi.a.partanen@aalto.fi>
2019-02-27 14:08:42 +0200

    Swedish id was incorrect, now fixed.

--- src/index.jsx
+++ src/index.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline'
 import './index.css';
-import DEFAULT_LANG from './constants/defaults.js';
 import { addLocaleData, IntlProvider } from "react-intl";
 import locale_en from 'react-intl/locale-data/en';
 import locale_fi from 'react-intl/locale-data/fi';
-import locale_se from 'react-intl/locale-data/se';
+import locale_sv from 'react-intl/locale-data/sv';
 import messages_en from "./translations/en.json";
 import messages_fi from "./translations/fi.json";
-import messages_se from "./translations/se.json";
+import messages_sv from "./translations/sv.json";
 import App from './App';
 
-addLocaleData([...locale_en, ...locale_fi, ...locale_se]);
+addLocaleData([...locale_en, ...locale_fi, ...locale_sv]);
 
 const messages = {
     'fi': messages_fi,
-    'se': messages_se,
+    'sv': messages_sv,
     'en': messages_en
 };
 
