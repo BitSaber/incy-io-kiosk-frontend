2ac6ad3b4312dc228d7e723e60b3535ecbaa7227
tom railio <tom.railio@aalto.fi>
2019-02-27 12:46:41 +0200

    Added react intl, and one default translation to thank you page

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline'
-import './index.css'
+import './index.css';
+import DEFAULT_LANG from './constants/defaults.js';
+import { addLocaleData, IntlProvider } from "react-intl";
+
 
 import App from './App';
 
 ReactDOM.render(
     <React.Fragment>
         <CssBaseline />
-        <App />
+        <IntlProvider locale={DEFAULT_LANG}>
+            <App />
+        </IntlProvider>
     </React.Fragment>,
     document.getElementById('app')
 );
--- src/pages/ThankYouPage.jsx
+++ src/pages/ThankYouPage.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import {FormattedMessage} from 'react-intl';
 
 // TODO: attributes of class txt should be implemented with material UI
 const ThankYouPage = () => (
     <div>
         <div className="center-align"><img src="/planblogo_color.jpg" className="logo"></img> </div>
         <div className="answer-div">
-            <h2 className="txt" variant="h2">Kiitos palautteesta!</h2>
+            <h2 className="txt" variant="h2">
+                <FormattedMessage id="app.thankyou"
+                    defaultMessage="Thank you for the feedback!"
+                    description="Thank you text on thank you page"
+                    values={{ what: 'react-intl' }}
+                />
+            </h2>
         </div>
 
         <footer className="footer">
