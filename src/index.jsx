fcf1d4f2ac5c745be53df06fea8fb9e425bbc118
Anders Nylund <anders.nylund@aalto.fi>
2019-03-01 14:31:55 +0200

    Move intlReducer to own file

--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline'
-import { addLocaleData } from "react-intl";
-import { IntlProvider, intlReducer } from "react-intl-redux";
-import locale_en from 'react-intl/locale-data/en';
-import locale_fi from 'react-intl/locale-data/fi';
-import locale_sv from 'react-intl/locale-data/sv';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { IntlProvider } from 'react-intl-redux';
 
 import './index.css';
 import App from './App';
-import messages_en from "./translations/en.json";
-
-addLocaleData([...locale_en, ...locale_fi, ...locale_sv]);
+import intlReducer, { initialState } from './reducers/intlReducer';
 
 const reducer = combineReducers({
     intl: intlReducer
 });
 
-const initialState = {
-    intl: {
-        defaultLocale: 'en',
-        locale: 'en',
-        messages: messages_en
+const store = createStore(
+    reducer,
+    {
+        intl: initialState
     },
-};
-
-const store = createStore(reducer, initialState, composeWithDevTools());
+    composeWithDevTools()
+);
 
 ReactDOM.render(
     <Provider store={store}>
