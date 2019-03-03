c16f1bfb8527ee43856730e7855f465613ca5148
Anders Nylund <anders.nylund@aalto.fi>
2019-03-03 09:33:08 +0200

    redux-answers. Add comment about deep-freeze

--- __tests__/answerReducer.test.js
+++ __tests__/answerReducer.test.js
@@ -3,6 +3,10 @@ import deepFreeze from 'deep-freeze';
 import answerReducer from '../src/reducers/answerReducer';
 import { addAnswerAction, resetAnswersAction } from '../src/actions/answerActions';
 
+/**
+ * Using deep-freeze allows to test that the reducer is NOT mutating the state
+ * https://twitter.com/dan_abramov/status/659417780236742656?lang=en
+ */
 
 describe('answerReducer', () => {
     it('should set the initial state', () => {
