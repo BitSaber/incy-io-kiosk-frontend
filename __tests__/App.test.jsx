5d2547e0bd581cf0cef913f3d2f8bf7016b1eb30
Anders Nylund <anders.nylund@aalto.fi>
2019-03-03 20:15:57 +0200

    redux-questions. Fix unit tests

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { App } from '../src/App';
+import App from '../src/App';
 
 describe('<App />', () => {
     it('should set the initial state correctly in constructor.', () => {
-        const newApp = shallow(<App currentLanguageId="en" answers={{}} addAnswer={jest.fn()} resetAnswers={jest.fn()} />);
-        expect(newApp.state().questions).toEqual([]);
-        expect(newApp.state().currentQuestionID).toEqual(null);
-        expect(newApp.state().currentQuestionChoices).toEqual([]);
-        expect(newApp.state().isAllQuestionsAnswered).toEqual(false);
-    })
+        const getQuestions = jest.fn();
+
+        const component = shallow(
+            <App
+                currentLanguageId="en"
+                answers={{}}
+                addAnswer={jest.fn()}
+                resetAnswers={jest.fn()}
+                getQuestions={getQuestions}
+                questions={{ allQuestions: [] }}
+            />
+        );
+        expect(component.state().currentQuestionID).toEqual(null);
+        expect(component.state().currentQuestionChoices).toEqual([]);
+        expect(component.state().isAllQuestionsAnswered).toEqual(false);
+    });
+
+    it('should call getQuestions on componentDidMount()', () => {
+        const getQuestions = jest.fn();
+        shallow(
+            <App
+                currentLanguageId="en"
+                answers={{}}
+                addAnswer={jest.fn()}
+                resetAnswers={jest.fn()}
+                getQuestions={getQuestions}
+                questions={{ allQuestions: [] }}
+            />
+        );
+        expect(getQuestions.mock.toBeCalled)
+    });
 })
--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const initialState = {
     error: null
 }
 
-export class App extends React.Component {
+class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = { ...initialState };
