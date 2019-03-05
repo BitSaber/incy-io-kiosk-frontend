cb99da994ece4e3435844d9586b2693b7a7c789c
Sebastian Berner <basti_@hotmail.com>
2019-03-05 16:06:55 +0200

    started refactoring textfield

---
+++ src/components/FreeText.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { FormattedMessage } from 'react-intl';
+import { TextField } from '@material-ui/core';
+
+const label = (<FormattedMessage id="textfield.placeholder"
+    defaultMessage="Your answer"
+    description="Placeholder on text field"
+    values={{ what: 'react-intl' }}
+/>);
+
+class FreeText extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: '' // current text of the textField
+        };
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            text: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div className="center-align txt">
+                <form>
+                    <TextField
+                        id="outlined-bare"
+                        label={label}
+                        multiline
+                        rows="20"
+                        margin="normal"
+                        value={this.state.text}
+                        onChange={this.handleChange}
+                        variant="outlined"
+                        style={{ width: 500 }}
+                    />
+                </form>
+            </div>
+        )
+    };
+}
+
