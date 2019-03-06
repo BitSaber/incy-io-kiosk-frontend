9c46bfdd4904063683b21d9797c8506419bb95f9
Valtteri Kortteisto <valtteri.kortteisto@gmail.com>
2019-03-06 15:01:24 +0200

    changed questions to be sorted based on position in questionReducer

--- src/actions/questionActions.js
+++ src/actions/questionActions.js
@@ -4,7 +4,7 @@ import service from '../service';
 export const setQuestionsAction = (langId) => {
     return async (dispatch) => {
         const questions = await service.getQuestions(langId);
-        const questionsSorted = questions.sort((object1, object2) => object1.id - object2.id)
+        const questionsSorted = questions.sort((object1, object2) => object1.position - object2.position)
         dispatch({
             type: SET_QUESTIONS,
             payload: questionsSorted,
