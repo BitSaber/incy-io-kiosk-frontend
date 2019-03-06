3e07f1a240141cc8ecafb0b61ccdae9574a3c083
Anders Nylund <anders.nylund@aalto.fi>
2019-03-06 18:20:51 +0200

    current-question-reducer. Move current question from App state to question reducer

--- src/reducers/questionReducer.js
+++ src/reducers/questionReducer.js
@@ -1,15 +1,23 @@
-import { SET_QUESTIONS } from '../actions/questionActions';
+import { SET_QUESTIONS, SET_CURRENT_QUESTION } from '../actions/questionActions';
 
 const initialState = {
     allQuestions: [],
-}
+    currentQuestion: null,
+};
 
 const reducer = (state=initialState, action) => {
     if (action.type === SET_QUESTIONS) {
         return {
             ...state,
-            allQuestions: action.payload
-        }
+            allQuestions: action.payload,
+        };
+    }
+
+    if (action.type === SET_CURRENT_QUESTION) {
+        return {
+            ...state,
+            currentQuestion: action.payload,
+        };
     }
 
     return state;
