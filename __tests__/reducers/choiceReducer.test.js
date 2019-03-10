cab406f488464ee0f0f9544ccd9c3531b6e5da55
Anders Nylund <anders.nylund@aalto.fi>
2019-03-10 13:23:35 +0200

    multi-select. Fix tests

--- __tests__/reducers/choiceReducer.test.js
+++ __tests__/reducers/choiceReducer.test.js
@@ -5,18 +5,21 @@ describe('choiceReducer', () => {
     it('should set the initial state', () => {
         const state = reducer(undefined, { type: 'TEST' });
         expect(state).toEqual({
-            availableChoices: []
+            availableChoices: [],
+            selectedChoices: [],
         });
     })
 
     it('should set the choices', () => {
         const state = reducer(undefined, { type: SET_AVAILABLE_CHOICES, payload: [{ id: 1 }] });
         expect(state).toEqual({
-            availableChoices: [{ id: 1 }]
+            availableChoices: [{ id: 1 }],
+            selectedChoices: [],
         });
         const nextState = reducer(state, { type: SET_AVAILABLE_CHOICES, payload: [{ id: 2 }] });
         expect(nextState).toEqual({
-            availableChoices: [{ id: 2 }]
+            availableChoices: [{ id: 2 }],
+            selectedChoices: [],
         });
     });
 });
