1d92cf1915fc2ed0a8703c28f7c5c74e1b161dac
Axel Latvala <axel@latvala.com>
2019-03-10 15:54:40 +0200

    Corrected path in tests

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../src/App';
+import App from '../src/components/App';
 
 // we mock the service so that we can return custom data
 jest.mock("../src/service", () => {
