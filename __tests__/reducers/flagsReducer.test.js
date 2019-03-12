91fe507616ca574f53f34cf869e77a4aff00d34b
Valtteri Kortteisto <valtteri.kortteisto@gmail.com>
2019-03-12 17:30:31 +0200

    fixed tests but still giving console errors for some props being required

--- __tests__/reducers/flagsReducer.test.js
+++ __tests__/reducers/flagsReducer.test.js
@@ -1,43 +1,69 @@
 import flagsReducer from '../../src/reducers/flagsReducer';
-import { setAllAnsweredAction, setAllDisplayedAction, setShowErrorAction } from '../../src/actions/flagActions';
+import { setAllAnsweredAction, setAllDisplayedAction, 
+    setShowErrorAction, setErrorMsgAction } from '../../src/actions/flagActions';
 
 describe('flagsReducer', () => {
     it('should set the initial state', () => {
-        const state = flagsReducer(undefined, { type: 'TEST_ACTION' } );
+        const state = flagsReducer(undefined, { type: 'TEST_ACTION' });
         expect(state).toEqual({
             isAllQuestionsAnswered: false,
             isAllQuestionsDisplayed: false,
-            showError: false,
+            error: {
+                showError: false,
+                message: "",
+            }
         });
     })
 
     it('should change isAllQuestionsAnswered to true', () => {
-        const state = flagsReducer(undefined, { type: 'TEST_ACTION' } );
-        const nextState = flagsReducer(state, setAllAnsweredAction(true) );
+        const state = flagsReducer(undefined, { type: 'TEST_ACTION' });
+        const nextState = flagsReducer(state, setAllAnsweredAction(true));
         expect(nextState).toEqual({
             isAllQuestionsAnswered: true,
             isAllQuestionsDisplayed: false,
-            showError: false,
+            error: {
+                showError: false,
+                message: "",
+            }
         });
     })
 
     it('should change isAllQuestionsDisplayed to true', () => {
-        const state = flagsReducer(undefined, { type: 'TEST_ACTION' } );
-        const nextState = flagsReducer(state, setAllDisplayedAction(true) );
+        const state = flagsReducer(undefined, { type: 'TEST_ACTION' });
+        const nextState = flagsReducer(state, setAllDisplayedAction(true));
         expect(nextState).toEqual({
             isAllQuestionsAnswered: false,
             isAllQuestionsDisplayed: true,
-            showError: false,
+            error: {
+                showError: false,
+                message: "",
+            }
         });
     })
 
     it('should change showError to true', () => {
-        const state = flagsReducer(undefined, { type: 'TEST_ACTION' } );
-        const nextState = flagsReducer(state, setShowErrorAction(true) );
+        const state = flagsReducer(undefined, { type: 'TEST_ACTION' });
+        const nextState = flagsReducer(state, setShowErrorAction(true));
         expect(nextState).toEqual({
             isAllQuestionsAnswered: false,
             isAllQuestionsDisplayed: false,
-            showError: true,
+            error: {
+                showError: true,
+                message: "",
+            }
+        });
+    })
+
+    it('should change error.message to `test`', () => {
+        const state = flagsReducer(undefined, { type: 'TEST_ACTION' });
+        const nextState = flagsReducer(state, setErrorMsgAction("test"));
+        expect(nextState).toEqual({
+            isAllQuestionsAnswered: false,
+            isAllQuestionsDisplayed: false,
+            error: {
+                showError: false,
+                message: "test",
+            }
         });
     })
 })
