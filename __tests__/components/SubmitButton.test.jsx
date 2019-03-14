73991ad4ae5f0c870d2650c378fbc7d3c2e7a56b
jallusparta <aleksi.a.partanen@aalto.fi>
2019-03-14 15:56:25 +0200

    Implemented submit buttons tests with finnish translation, and also switched the translation of skip button tests to finnish, as swedish and english translations match with the default value.

--- __tests__/components/SubmitButton.test.jsx
+++ __tests__/components/SubmitButton.test.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import { mountWithIntl, loadTranslationObject } from 'enzyme-react-intl';
 import Button from '@material-ui/core/Button';
 import fi from '../../src/translations/fi';
-import SkipButton from '../../src/components/SkipButton';
+import SubmitButton from '../../src/components/SubmitButton';
 
 
 loadTranslationObject(fi);
-describe('<SkipButton />', () => {
+describe('<SubmitButton />', () => {
 
     it('should render the default text from Intl in finnish', () => {
-        const component = mountWithIntl(<SkipButton onClick={jest.fn()} />);
-        expect(component.html()).toContain(fi.skipbutton);
+        const component = mountWithIntl(<SubmitButton onClick={jest.fn()} />);
+        expect(component.html()).toContain(fi.submitbutton);
     });
 
     it('should call the clickHandler', () => {
         const clickHandler = jest.fn();
-        const component = mountWithIntl(<SkipButton onClick={clickHandler} />);
+        const component = mountWithIntl(<SubmitButton onClick={clickHandler} />);
         const button = component.find(Button);
         button.props().onClick();
         expect(clickHandler).toBeCalled();
