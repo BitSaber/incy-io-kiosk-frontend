c389ce743abb74e4fbc52ded406c95017f40b364
jallusparta <aleksi.a.partanen@aalto.fi>
2019-03-14 14:19:33 +0200

    A test on app I tried to make. Still no idea about how I should test the react applications.

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -26,8 +26,10 @@ jest.mock("../src/service", () => {
 });
 
 describe('<App />', () => {
-    it('should call setQuestions on componentDidMount()', () => {
+    it('should call setQuestions, setCategory and setPlace on componentDidMount()', () => {
         const setQuestions = jest.fn();
+        const setCategory = jest.fn();
+        const setPlace = jest.fn();
         shallow(
             <App
                 currentLanguageId="en"
@@ -39,8 +41,8 @@ describe('<App />', () => {
                 setAvailableChoices={jest.fn()}
                 questions={{ allQuestions: [] }}
                 context={{ place: [], category: [] }}
-                setPlace={jest.fn()}
-                setCategory={jest.fn()}
+                setPlace={setPlace}
+                setCategory={setCategory}
                 setAllAnswered={jest.fn()}
                 setAllDisplayed={jest.fn()}
                 flags={{
@@ -55,5 +57,41 @@ describe('<App />', () => {
             />
         );
         expect(setQuestions.mock.toBeCalled)
+        expect(setCategory.mock.toBeCalled)
+        expect(setPlace.mock.toBeCalled)
     });
+
+    it('should set the question on position 1 as the current question.', () =>{
+        const setQuestions = jest.fn();
+        const setCategory = jest.fn();
+        const setPlace = jest.fn();
+        shallow(
+            <App
+                currentLanguageId="en"
+                answers={{}}
+                addAnswer={jest.fn()}
+                resetAnswers={jest.fn()}
+                setQuestions={setQuestions}
+                setCurrentQuestion={jest.fn()}
+                setAvailableChoices={jest.fn()}
+                questions={{ allQuestions: [], currentQuestion: {} }}
+                context={{ place: [], category: [] }}
+                setPlace={setPlace}
+                setCategory={setCategory}
+                setAllAnswered={jest.fn()}
+                setAllDisplayed={jest.fn()}
+                flags={{
+                    isAllQuestionsAnswered: false,
+                    isAllQuestionsDisplayed: false,
+                    error: {  showError: false, messageId: "" }
+                }}
+                setShowError={jest.fn()}
+                setErrorMsg={jest.fn()}
+                choices={{ availableChoices: [] }}
+                resetText={jest.fn()}
+            />
+        );
+        expect(questions.currentQuestion).toEqual({position: 1})
+    });
+    })
 })
