fbf38b0e6f90091c464adccfb83073d8bef36899
Anders Nylund <anders.nylund@aalto.fi>
2019-03-16 10:35:32 +0200

    INCYKIOSK-84. Filter out questions that are not in category's questions

--- src/actions/questionActions.js
+++ src/actions/questionActions.js
@@ -3,12 +3,22 @@ import service from '../service';
 import { SET_QUESTIONS, SET_CURRENT_QUESTION } from '../constants/actions';
 
 export const setQuestionsAction = (langId) => {
-    return async (dispatch) => {
+    return async (dispatch, getState) => {
+        const { context } = getState();
+        const categoryQuestionIDs = context.category[0].question_ids;
+
+        // get all questions
         const questions = await service.getQuestions(langId);
-        const questionsSorted = questions.sort((object1, object2) => object1.position - object2.position);
+
+        // sort them by their position
+        const sorted = questions.sort((object1, object2) => object1.position - object2.position);
+
+        // filter out questions that are not in this category's list of questions
+        const filtered = sorted.filter(question => categoryQuestionIDs.includes(question.id));
+
         dispatch({
             type: SET_QUESTIONS,
-            payload: questionsSorted,
+            payload: filtered,
         });
     };
 };
