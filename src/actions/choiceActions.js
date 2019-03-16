efa5c8a754d1c08c392272dee9c1d8864747317a
Valtteri Kortteisto <valtteri.kortteisto@gmail.com>
2019-03-16 14:20:41 +0200

    added a loadingState object into store, which keeps track of the loading state of questions and choices separately, loadingActions.js is not being used anywhere, and might not need to be

--- src/actions/choiceActions.js
+++ src/actions/choiceActions.js
@@ -1,24 +1,34 @@
 import service from '../service';
-import { RESET_ALL_CHOICES, GET_CHOICES, SET_CURRENT_CHOICES, SET_SELECTED_CHOICES } from '../constants/actions';
-
+import { RESET_ALL_CHOICES, GET_CHOICES, SET_CURRENT_CHOICES,
+    SET_SELECTED_CHOICES, SET_CHOICES_LOADING_STATE } from '../constants/actions';
+import { LOADING_STATE, FINISHED_STATE } from '../constants/loadingStates';
 
 export const getAllChoicesAction = (questions, langId) => {
     return async (dispatch) => {
+        // set the lodaing state to load
+        dispatch({
+            type: SET_CHOICES_LOADING_STATE,
+            payload: LOADING_STATE,
+        });
         // we first reset the choices incase we are changing language
         dispatch({
             type: RESET_ALL_CHOICES,
         });
         // here we use a for-loop to go through all questions (preferably sorted in some fashion),
         // and dispatch them one by one to an array with all the possible choices
-        var i;
         const qLen = questions.length;
-        for (i = 0; i < qLen; i++) {
+        for (let i = 0; i < qLen; i++) {
             const choices = await service.getChoices(questions[i].id, langId);
             dispatch({
                 type: GET_CHOICES,
                 payload: choices,
             });
         }
+        // set the lodaing state to be finished
+        dispatch({
+            type: SET_CHOICES_LOADING_STATE,
+            payload: FINISHED_STATE,
+        });
     };
 };
 
--- src/actions/questionActions.js
+++ src/actions/questionActions.js
@@ -1,14 +1,25 @@
 import service from '../service';
 
-import { SET_QUESTIONS, SET_CURRENT_QUESTION } from '../constants/actions';
+import { SET_QUESTIONS, SET_CURRENT_QUESTION, SET_QUESTIONS_LOADING_STATE } from '../constants/actions';
+import { LOADING_STATE, FINISHED_STATE } from '../constants/loadingStates';
 
 export const setQuestionsAction = (langId) => {
     return async (dispatch) => {
+        // set the lodaing state to load
+        dispatch({
+            type: SET_QUESTIONS_LOADING_STATE,
+            payload: LOADING_STATE,
+        });
         const questions = await service.getQuestions(langId);
         const questionsSorted = questions.sort((object1, object2) => object1.position - object2.position);
         dispatch({
             type: SET_QUESTIONS,
             payload: questionsSorted,
+        });
+        // set the lodaing state to be finished
+        dispatch({
+            type: SET_QUESTIONS_LOADING_STATE,
+            payload: FINISHED_STATE,
         });
     };
 };
