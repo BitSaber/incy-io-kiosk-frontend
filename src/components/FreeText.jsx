7062173b9d8cd96340bb3149604d9328c9867457
Emmi Mannonen <emmi.mannonen@aalto.fi>
2019-03-20 13:40:20 +0200

    Fixing some placeholdershit

--- src/components/FreeText.jsx
+++ src/components/FreeText.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
-// import { FormattedMessage } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 import { TextField } from '@material-ui/core';
 import { string, func } from 'prop-types';
-<<<<<<< HEAD
-import PropTypes from 'prop-types';
-import TextField from '@material-ui/core/TextField';
-import { withStyles } from '@material-ui/core/styles';
-
-
-=======
 import Grid from '@material-ui/core/Grid';
->>>>>>> 36ea3350457c0595334d8aeaff65eeece02eee7c
 
 const style = {
     textDiv: {
@@ -36,18 +28,14 @@ export default class FreeText extends React.Component {
     }
 
     render() {
-<<<<<<< HEAD
-        const { classes } = this.props;
-=======
+        const label = (
+             <FormattedMessage id="textfield.placeholder"
+                 defaultMessage="Your answer"
+                 description="Placeholder on text field"
+                 values={{ what: 'react-intl' }}
+             />
+        );
 
-        // const label = (
-        //     <FormattedMessage id="textfield.placeholder"
-        //         defaultMessage="Your answer"
-        //         description="Placeholder on text field"
-        //         values={{ what: 'react-intl' }}
-        //     />
-        // );
->>>>>>> 36ea3350457c0595334d8aeaff65eeece02eee7c
 
         return (
             <Grid >
@@ -57,14 +45,6 @@ export default class FreeText extends React.Component {
                         multiline
                         rows="10"
                         margin="normal"
-<<<<<<< HEAD
-                        placeholder="Placeholder"
-                        InputLabelProps={{
-                          shrink: true,
-                        }}
-=======
-                        placeholder="Write something nice"
->>>>>>> 36ea3350457c0595334d8aeaff65eeece02eee7c
                         value={this.props.text}
                         onChange={this.props.handleChange}
                         InputProps={{ disableUnderline: true, style: { fontSize: 30, color: '#ffffff', hover: '#ffffff' } }}
