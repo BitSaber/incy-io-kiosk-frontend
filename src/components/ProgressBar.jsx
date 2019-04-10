7f6df09c52b9676709c652e90fbc75cda62a126a
Axel Latvala <axel@latvala.com>
2019-04-10 22:16:17 +0300

    Progress bar color through prop (#133)
    * Progress bar color through prop
    
    * Improved hackyness rating of 'fix', which corrects the symptoms
    displayed in the previous commit
    
    * Corrected typo

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { number, shape, string } from 'prop-types';
+import { number, shape, string, oneOfType } from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
 const styles = {
     bar: {
-        height: 18,
-        background: '#0078CC',
+        height: 32,
+        backgroundColor: '#0078CC',
     },
 };
 
@@ -15,20 +15,41 @@ class ProgressBar extends React.Component {
         super(props);
     }
 
+    componentDidMount() {
+        // XXX: Mega-hack. Current version of LinearProgress does not support
+        // what we are about to do:
+        // if we have specified barColor, insert CSS for it, given that we haven't
+        // aleady done this hack
+        const styleElId = 'haxed-progressbar-color';
+        if(document.querySelectorAll(`#${styleElId}`).length < 1
+            && ['string', 'number'].indexOf(typeof this.props.barColor) !== -1) {
+            const style = document.createElement('style');
+            style.setAttribute('id', 'haxed-progressbar-color');
+            style.innerHTML = '#linearProgressBar01 div { background-color: '+this.props.barColor+' !important }';
+            document.head.appendChild(style);
+        }
+    }
+
     static propTypes = {
         progress: number.isRequired,
         classes: shape({
             bar: string.isRequired,
         }),
+        barColor: oneOfType([
+            string,
+            number,
+        ]),
     }
 
     render() {
+
         const { classes, progress } = this.props;
         return (
             <div className={classes.root}>
                 <LinearProgress variant="determinate"
                     value={progress}
                     className={classes.bar}
+                    id={'linearProgressBar01'}
                 />
             </div>
         );
